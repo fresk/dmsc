d90ce34952359db26d9e8eb676bef5f70159c0da
hansent <thomas.hansen@gmail.com>
2014-03-01 11:09:11 -0600

    delete routes folder

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+  res.render('index', { title: 'DMSC'});
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
